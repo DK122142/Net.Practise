ec3ae88ae27f72eec62b84646be352ba99d95781
DmitriyK42 <dmitriykoval00@gmail.com>
2021-01-15 11:10:18 +0200

    project initialization

---
+++ DI.App/Program.cs
@@ -0,0 +1,23 @@
+﻿using DI.App.Services.PL;
+
+namespace DI.App
+{
+    interface IRepository<T>
+    {
+
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            // Inversion of Control
+            var manager = new CommandManager();
+
+            manager.Start();
+
+
+
+        }
+    }
+}
