9f6bcaee4457f0e51771fe25ed98c1da40f9250a
DmitriyK42 <dmitriykoval00@gmail.com>
2021-01-15 10:35:27 +0200

    using app in console

---
+++ FileManager/Input.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FileManager
+{
+    public static class Input
+    {
+        public static Explorer Explorer { get; set; }
+
+        public static void Start()
+        {
+            while (Process()) ;
+        }
+
+        public static bool Process()
+        {
+            try
+            {
+                var cmd = Console.ReadLine() ?? string.Empty;
+
+                Explorer.Execute(cmd);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e}. Message: {e.Message}");
+
+                return false;
+            }
+        }
+
+    }
+}
---
+++ FileManager/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace FileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Input.Explorer = new Explorer();
+
+            Input.Start();
+        }
+    }
+}
