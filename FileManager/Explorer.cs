8fb48c79c495e11508dd6490a3cbed18409a7e76
DmitriyK42 <dmitriykoval00@gmail.com>
2021-01-15 10:34:54 +0200

    explorer functionality

---
+++ FileManager/Explorer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+
+namespace FileManager
+{
+    public class Explorer
+    {
+        public ExplorerService ExplorerService { get; set; }
+
+        public Explorer()
+        {
+            this.ExplorerService = new ExplorerService();
+            
+            Console.Write($"{ExplorerService.CurrentDirectory} ");
+        }
+
+        public void ShowContent()
+        {
+            Console.Clear();
+
+            ExplorerService.ShowDirectoryContent();
+
+            Console.WriteLine();
+            Console.Write($"{ExplorerService.CurrentDirectory} ");
+        }
+
+        public void GoTo(string path)
+        {
+            Console.Clear();
+
+            if (path.Equals(".."))
+            {
+                ExplorerService.GoBack();
+            }
+            else
+            {
+                ExplorerService.SetDirectory(path);
+            }
+            
+            Console.Write($"{ExplorerService.CurrentDirectory} ");
+        }
+
+        public void ReadFile(string path)
+        {
+            Console.Clear();
+
+            if (FileReader.IsTxt(path))
+            {
+                new FileReader().ReadTxt(path);
+            }
+            else
+            {
+                new FileReader().ReadAnotherFile(path);
+            }
+
+            Console.Write($"{ExplorerService.CurrentDirectory} ");
+        }
+
+        public void Execute(string input)
+        {
+            var args = input.Split(" ");
+
+            switch (args.FirstOrDefault())
+            {
+                case "cd":
+                    GoTo(args[1]);
+                    break;
+                case "r":
+                    ReadFile(args[1]);
+                    break;
+                case "ls":
+                    ShowContent();
+                    break;
+                case "exit":
+                    Environment.Exit(0);
+                    break;
+                case "":
+                    Console.Write($"{ExplorerService.CurrentDirectory} ");
+                    break;
+                default:
+                    Console.WriteLine("Unavailable operation");
+                    break;
+            }
+        }
+
+    }
+}
---
+++ FileManager/ExplorerService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FileManager
+{
+    public class ExplorerService
+    {
+        public string CurrentDirectory { get; private set; }
+
+        public ExplorerService()
+        {
+            SetDirectory(@"C:\");
+        }
+
+        public void SetDirectory(string directoryPath)
+        {
+            Directory.SetCurrentDirectory(directoryPath);
+            CurrentDirectory = Directory.GetCurrentDirectory();
+        }
+
+        public void GoBack()
+        {
+            var directoryInfo = new DirectoryInfo(Directory.GetCurrentDirectory()).Parent;
+
+            if (directoryInfo != null)
+            {
+                SetDirectory(directoryInfo.FullName);
+            }
+            else
+            {
+                Console.WriteLine("No parent for this directory");
+            }
+        }
+
+        public void ShowDirectoryContent()
+        {
+            var directories = Directory.EnumerateDirectories(CurrentDirectory)
+                .Where(d => !new DirectoryInfo(d).Attributes.HasFlag(FileAttributes.Hidden));
+
+            var files = Directory.EnumerateFiles(CurrentDirectory)
+                .Where(d => !new FileInfo(d).Attributes.HasFlag(FileAttributes.Hidden));
+
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory.Replace(CurrentDirectory, string.Empty)
+                    .Replace("\\", string.Empty));
+            }
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file.Replace(CurrentDirectory, string.Empty)
+                    .Replace("\\", string.Empty));
+            }
+        }
+    }
+}
