bcb80a056317f415cff674961fa86a0d893600a2
DmitriyK42 <dmitriykoval00@gmail.com>
2021-01-25 14:32:59 +0200

    added unit tests

--- SomeBusinessService.Test/MainBusinessLogicServiceTest.cs
+++ SomeBusinessService.Test/MainBusinessLogicServiceTest.cs
@@ -33,9 +33,17 @@ namespace SomeBusinessService.Test
             var service = new MainBusinessLogicService(dbMangerMock.Object);
             var newProduct = new Product();
 
-            newProduct.Id = Guid.NewGuid();
-            newProduct.Count = 12;
-            newProduct.LastUpdated = DateTime.Now;
+            Assert.Throws<ArgumentException>(() => service.Create(newProduct));
+        }
+
+        [Test]
+        public void Create_Product_ArgumentExceptionDueToIsEmpty()
+        {
+            var dbMangerMock = new Mock<IDBManager>();
+            var service = new MainBusinessLogicService(dbMangerMock.Object);
+            var newProduct = new Product();
+
+            newProduct.Name = string.Empty;
 
             Assert.Throws<ArgumentException>(() => service.Create(newProduct));
         }
@@ -53,6 +61,17 @@ namespace SomeBusinessService.Test
         }
 
         [Test]
+        public void Delete_Name_NothingWhenNameNull()
+        {
+            var dbMangerMock = new Mock<IDBManager>();
+            var service = new MainBusinessLogicService(dbMangerMock.Object);
+
+            service.Delete(null);
+
+            dbMangerMock.Verify(db => db.Delete(null), Times.Never);
+        }
+
+        [Test]
         public void Delete_Name_NothingWhenNameEmpty()
         {
             var dbMangerMock = new Mock<IDBManager>();
@@ -61,7 +80,86 @@ namespace SomeBusinessService.Test
 
             service.Delete(productName);
 
-            dbMangerMock.Verify(db => db.Delete(It.IsAny<string>()), Times.Never);
+            dbMangerMock.Verify(db => db.Delete(productName), Times.Never);
+        }
+
+        [Test]
+        public void Get_Name_DbManagerCallGet()
+        {
+            var dbMangerMock = new Mock<IDBManager>();
+            var service = new MainBusinessLogicService(dbMangerMock.Object);
+            var productName = "TestName1";
+
+            service.Get(productName);
+
+            dbMangerMock.Verify(db => db.Get(productName), Times.Once);
+        }
+
+        [Test]
+        public void Get_Name_ArgumentNullExceptionWhenNameIsEmpty()
+        {
+            var dbMangerMock = new Mock<IDBManager>();
+            var service = new MainBusinessLogicService(dbMangerMock.Object);
+
+            var productName = string.Empty;
+
+            Assert.Throws<ArgumentNullException>(() => service.Get(productName));
+        }
+
+        [Test]
+        public void Get_Name_ArgumentNullExceptionWhenNameIsNull()
+        {
+            var dbMangerMock = new Mock<IDBManager>();
+            var service = new MainBusinessLogicService(dbMangerMock.Object);
+
+            Assert.Throws<ArgumentNullException>(() => service.Get(null));
+        }
+
+        [Test]
+        public void Update_ProductName_DbManagerCallUpdate()
+        {
+            var dbMangerMock = new Mock<IDBManager>();
+            var service = new MainBusinessLogicService(dbMangerMock.Object);
+            var product = new Product();
+            product.Name = "TestName1";
+            product.LastUpdated = DateTime.MinValue;
+
+            service.Update(product, product.Name);
+
+            dbMangerMock.Verify(db => db.Update(product, product.Name), Times.Once);
+        }
+        
+        [Test]
+        public void Update_ProductName_ArgumentNullExceptionWhenNameIsEmpty()
+        {
+            var dbMangerMock = new Mock<IDBManager>();
+            var service = new MainBusinessLogicService(dbMangerMock.Object);
+            var product = new Product();
+            product.Name = string.Empty;
+            
+            Assert.Throws<ArgumentNullException>(() => service.Update(product, product.Name));
+        }
+        
+        [Test]
+        public void Update_ProductName_ArgumentNullExceptionWhenNameIsNull()
+        {
+            var dbMangerMock = new Mock<IDBManager>();
+            var service = new MainBusinessLogicService(dbMangerMock.Object);
+            var product = new Product();
+
+            Assert.Throws<ArgumentNullException>(() => service.Update(product, product.Name));
+        }
+        
+        [Test]
+        public void Update_ProductName_ArgumentNullExceptionWhenLastUpdatedBiggerThenNow()
+        {
+            var dbMangerMock = new Mock<IDBManager>();
+            var service = new MainBusinessLogicService(dbMangerMock.Object);
+            var product = new Product();
+            product.Name = "TestName1";
+            product.LastUpdated = DateTime.MaxValue;
+
+            Assert.Throws<ArgumentNullException>(() => service.Update(product, product.Name));
         }
     }
 }
