58ed20a3d5ae4b8b1e5742aede996ddeaa4bf8fb
DmitriyK42 <dmitriykoval00@gmail.com>
2021-03-10 11:10:55 +0200

    mapping

---
+++ PhoneBook/ViewModels/PhoneNumberViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.ViewModels
+{
+    public class PhoneNumberViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Address { get; set; }
+
+        public string Number { get; set; }
+
+        public DateTime Added { get; set; }
+
+        public DateTime Updated { get; set; }
+
+        public string Status { get; set; }
+
+        public string CreatorId { get; set; }
+    }
+}
